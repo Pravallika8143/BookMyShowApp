f9d2f3872bdd39aac4d65e075da0381d14baa2a8
Pravallika8143 <mamidisettipravallika4@gmail.com>
2025-09-24 16:27:15 +0530

    signup

--- src/assets/features/customer/Signup.jsx
+++ src/assets/features/customer/Signup.jsx
@@ -11,7 +11,7 @@ function Signup() {
            }
        })
      return (
-       <div style={{ maxWidth: 400,margin: "2rem auto", padding:24, border:"1px solid #9b9898ff",borderRadius: 8}}>
+       <div style={{ maxWidth: 350,margin: "2rem auto", padding:24, border:"1px solid #9b9898ff",borderRadius: 8}}>
            <h2 className='mb-4'>User Signup</h2>
            <form onSubmit={signupForm.handleSubmit}>
             <label className='mb-1 '>Username:</label><br/>
@@ -20,7 +20,7 @@ function Signup() {
                <input type='text' {...signupForm.getFieldProps('password')} placeholder='Enter password'/><br/><br/>
             <label className='mb-1 '>Mobile Number:</label><br/>
                <input type='text' {...signupForm.getFieldProps('mobile')} placeholder='Mobile Number'/><br/><br/>
-               <button className='btn btn-primary'>Submit</button>
+               <button className='btn btn-primary ms-5'>Submit</button>
            </form>
        </div>
      )
