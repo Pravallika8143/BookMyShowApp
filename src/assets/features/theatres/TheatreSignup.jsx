d84bebc974d4f7def1476a31fbe20e54c5c50442
Pravallika8143 <mamidisettipravallika4@gmail.com>
2025-09-24 16:14:16 +0530

    theatresignup

--- src/assets/features/theatres/TheatreSignup.jsx
+++ src/assets/features/theatres/TheatreSignup.jsx
@@ -11,7 +11,18 @@ function TheatreSignup() {
         }
     })
   return (
-    
+    <div>
+        <h2 className='mb-4'>Theatre Signup</h2>
+        <form onSubmit={theatreForm.handleSubmit}>
+        <label className='mb-1 '>Username:</label><br/>
+            <input type='text' {...theatreForm.getFieldProps('username')} placeholder='Enter your name'/><br/><br/>
+        <label className='mb-1 '>Password:</label><br/>
+            <input type='text' {...theatreForm.getFieldProps('password')} placeholder='Enter password'/><br/><br/>
+        <label className='mb-1 '>Mobile Number:</label><br/>
+            <input type='text' {...theatreForm.getFieldProps('mobile')} placeholder='Mobile Number'/><br/><br/>
+        <button className='ms-5'>Submit</button>
+        </form>
+    </div>
   )
 }
 
