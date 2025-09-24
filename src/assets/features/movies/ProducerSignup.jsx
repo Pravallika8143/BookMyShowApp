34feaee0dac8b3d1feafb8f51d7dc4b93e9dea5c
Pravallika8143 <mamidisettipravallika4@gmail.com>
2025-09-24 16:18:46 +0530

    producersignup

--- src/assets/features/movies/ProducerSignup.jsx
+++ src/assets/features/movies/ProducerSignup.jsx
@@ -20,7 +20,7 @@ function ProducerSignup() {
             <input type='text' {...producerForm.getFieldProps('password')} placeholder='Enter password'/><br/><br/>
         <label className='mb-1 '>Mobile Number:</label><br/>
             <input type='text' {...producerForm.getFieldProps('mobile')} placeholder='Mobile Number'/><br/><br/>
-        <button className='ms-5'>Submit</button>
+        <button className='btn btn-primary'>Submit</button>
         </form>
     </div>
   )
