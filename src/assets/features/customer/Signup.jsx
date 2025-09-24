761bef6b06f9f2a56ef4e0f90b07e6b317e7c3c3
Pravallika8143 <mamidisettipravallika4@gmail.com>
2025-09-24 16:18:18 +0530

    signup

--- src/assets/features/customer/Signup.jsx
+++ src/assets/features/customer/Signup.jsx
@@ -20,7 +20,7 @@ function Signup() {
                <input type='text' {...signupForm.getFieldProps('password')} placeholder='Enter password'/><br/><br/>
             <label className='mb-1 '>Mobile Number:</label><br/>
                <input type='text' {...signupForm.getFieldProps('mobile')} placeholder='Mobile Number'/><br/><br/>
-               <button className='ms-5'>Submit</button>
+               <button className='btn btn-primary'>Submit</button>
            </form>
        </div>
      )
