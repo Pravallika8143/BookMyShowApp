a3afd8768bb0e9f8226155eeab52054664eac114
Pravallika8143 <mamidisettipravallika4@gmail.com>
2025-09-24 15:46:12 +0530

    SignUp

--- src/assets/features/customer/Signup.jsx
+++ src/assets/features/customer/Signup.jsx
@@ -1,5 +1,5 @@
-import { useFormik } from 'formik'
-import React from 'react'
+import { useFormik } from 'formik';
+import React from 'react';
 
 function Signup() {
    var signupForm = useFormik({
